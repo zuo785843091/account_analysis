829e4aa26d900673261e4753e1ed23f28a35a7e8
zuo785843091 <785843091@qq.com>
2019-08-11 19:04:20 +0800

    V1.4.1
    1 修改了config.py，替换excel文件时不从config中修改文件名，修改excel文件名来与config中的文件名匹配。

--- configs.py
+++ configs.py
@@ -19,12 +19,12 @@ class configs(object):
         Constructor
         '''
         
-        self.account_analysis_book = r'往来分析表-201904（胡婷彬）.xls'
-        self.non_verification_book = r'201905-CUC_未核销预付款明细表_030619.xls'
-        self.payable_book = r'201905-CUC_应付帐款和其他应付帐款报表_030619.xls'
+        self.account_analysis_book = r'往来分析表.xls'
+        self.non_verification_book = r'未核销预付款明细表.xls'
+        self.payable_book = r'应付帐款和其他应付帐款报表.xls'
         
         self.template_sheet = '模板'
-        self.non_verification_sheet = '201905-CUC_未核销预付款明细表_030619'
-        self.payable_sheet = '201905-CUC_应付帐款和其他应付帐款报表_030619'
+        self.non_verification_sheet = '未核销预付款明细表'
+        self.payable_sheet = '应付帐款和其他应付帐款报表'
         self.MAX_ROW_NUM = 5000
         self.MAX_COL_NUM = 300
